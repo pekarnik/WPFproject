a8206359d820ac011c3b5dd3808c166b6fe2bd70
Pekarnik <pekgoglo@gmail.com>
2018-08-12 04:45:11 +0300

    Добавлены классы Отдел и Сотрудник

---
+++ WPF_Project/WPF_Project/Department.cs
@@ -0,0 +1,22 @@
+﻿namespace WPF_Project
+{
+    class Department
+    {
+        private string nameOfDepartment;
+        public string DepartmentName
+        {
+            get
+            {
+                return nameOfDepartment; 
+            }
+            protected set
+            {
+                nameOfDepartment = value;
+            }
+        }
+        public Department(string _nameOfDepartment)
+        {
+            DepartmentName = _nameOfDepartment;
+        }
+    }
+}
---
+++ WPF_Project/WPF_Project/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace WPF_Project
+{
+    class Employee:Department
+    {
+        private string nameOfEmployee;
+        public string EmployeeName
+        {
+            get=>nameOfEmployee;private set => nameOfEmployee = value;            
+        }
+        public Employee(string _nameOfEmployee,string _nameOfDepartment):base(_nameOfDepartment)
+        {
+            EmployeeName = _nameOfEmployee;
+        }
+    }
+}
